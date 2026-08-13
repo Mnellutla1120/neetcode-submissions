26fc2d2c535d18b7b009f2b2530f358f9669c7a9
Moukthika <162918815+Mnellutla1120@users.noreply.github.com>
2026-08-13 19:36:17 -0400

    Add: candy - submission-4

---
+++ Data Structures & Algorithms/candy/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+
+         ratings.sort()
+
+         candy_counter = len(ratings)
+         
+
+         for i in range(len(ratings)-1):
+            if abs(ratings[i] - ratings[i + 1]) > 0:
+                 candy_counter += 1
+         return candy_counter
