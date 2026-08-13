e01ab2d86409b09a85430d141be6526b6628d55f
Moukthika <162918815+Mnellutla1120@users.noreply.github.com>
2026-08-13 19:32:51 -0400

    Add: candy - submission-1

---
+++ Data Structures & Algorithms/candy/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        candy_count = len(ratings) #everyone gets one candy
+        #if rating of subsequent child is less than or equal to rating of prev child, they get less candy
+
+        for i in range(1,len(ratings)):
+            if abs(ratings[i - 1] - ratings[i]) >= 0:
+                 candy_count += 1
+
+        
+        return candy_count
+        
+        
