136f74da9fbbc9cf45a070a1920f2ec1363c47e2
Moukthika <162918815+Mnellutla1120@users.noreply.github.com>
2026-09-04 17:29:02 -0500

    Add: score-of-a-string - submission-2

---
+++ Data Structures & Algorithms/score-of-a-string/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        total = 0
+        c = 0
+        while c < len(s) - 1:
+             total += abs(ord(s[c]) - ord(s[c+1]))
+             c += 1
+        
+        
+        
+        return total
+
+
+        
