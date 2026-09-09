c49f813a0a76dad9fff454e71567010c791430fa
Moukthika <162918815+Mnellutla1120@users.noreply.github.com>
2026-09-09 18:54:31 -0500

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        count = 0
+        for c in details:
+            if int(c[11])*10 + int(c[12]) > 65:
+                count += 1
+        
+        return count
+                
