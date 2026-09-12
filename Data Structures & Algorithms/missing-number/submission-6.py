00b14675d1e7b8c894b5655e9032cade5f3dda83
Moukthika <162918815+Mnellutla1120@users.noreply.github.com>
2026-09-11 20:10:13 -0500

    Add: missing-number - submission-6

---
+++ Data Structures & Algorithms/missing-number/submission-6.py
@@ -0,0 +1,23 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        rang = len(nums)
+        l = 0
+        miss = 0
+
+        while l < rang:
+            if nums[l] != l:
+                return l
+            l += 1
+
+        return 0
+        
+    
+    
+        
+
+
+        
+
+
+        
